cb8a122223ad70c52da0ec022ba3a420cf710552
Tim Flood <tflood9@gmail.com>
2018-11-15 16:42:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,7 +32,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array
 }
 
-function removeElementFromBeginningofArray(array){
+function removeElementFromBeginningOfArray(array){
 array.slice(1)
 return array
 }
